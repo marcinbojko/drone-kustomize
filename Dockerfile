52d08c6ada14440a6ac89718a0257249808a0f98
Marcin Bojko <marcin@bojko.com.pl>
2022-08-26 07:50:35 +0200

    1.0.9

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-FROM marcinbojko/pipetools-k8s:v0.22.21 AS build
+FROM marcinbojko/pipetools-k8s:v0.25.23 AS build
 
-LABEL version="v1.0.5"
+LABEL version="v1.0.9"
 LABEL release="drone-kustomize"
 LABEL maintainer="marcinbojko"
 SHELL ["/bin/ash", "-euo", "pipefail", "-c"]
@@ -8,6 +8,9 @@ SHELL ["/bin/ash", "-euo", "pipefail", "-c"]
 # install kustomize
 RUN curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash && chmod +x ./kustomize && mv kustomize /usr/bin/kustomize \
 && kustomize version
+
+# install kubeconform
+RUN wget -qO- "https://github.com/yannh/kubeconform/releases/download/v0.4.14/kubeconform-linux-amd64.tar.gz" | tar xvz -C /usr/bin/
 
 # copy plugin.sh which contains deployment logic
 COPY plugin.sh /drone/
--- plugin.sh
+++ plugin.sh
@@ -1,30 +1,44 @@
 #!/usr/bin/env bash
 
 if [[ "$PLUGIN_VERSIONS" == "true" ]]; then
-    echo "Versions: helm        - $(helm version --client)"
-    echo "Versions: kubectl     - $(kubectl version --client)"
-    echo "Versions: kustomize   - $(kustomize version)"
+    echo "Versions: helm          - $(helm version --client)"
+    echo "Versions: kubectl       - $(kubectl version --client)"
+    echo "Versions: kustomize     - $(kustomize version)"
+    echo "Versions: kubeconform   - $(kubeconform -v)"
+    echo "Variables: PLUGIN_NAMESPACE - $PLUGIN_NAMESPACE"
 fi
 
 set -eu pipefail
 
 "${PLUGIN_DEBUG:-false}" && set -x
 
-
-if [ -z "$PLUGIN_KUBECONFIG" ] || [ -z "$PLUGIN_FOLDERPATH" ]; then
-    echo "KUBECONFIG and/or FILEPATH not supplied"
-    exit 1
+if [ "$PLUGIN_KUSTOMIZE_DEPLOY" == "true" ]; then
+    if [ -z "$PLUGIN_KUBECONFIG" ] || [ -z "$PLUGIN_FOLDERPATH" ]; then
+        echo "KUBECONFIG and/or FILEPATH not supplied"
+        exit 1
+    fi
+    if [ -n "$PLUGIN_KUBECONFIG" ];then
+        [ -d "$HOME"/.kube ] || mkdir "$HOME"/.kube  # uncomment post testing
+        echo "# Plugin PLUGIN_KUBECONFIG available" >&2
+        echo "$PLUGIN_KUBECONFIG" > "$HOME"/.kube/config # uncomment post testing
+        unset PLUGIN_KUBECONFIG
+    fi
+    [ -n "${PLUGIN_DEBUG:-false}" ] && kustomize build "${PLUGIN_FOLDERPATH}"
+    if [ "$PLUGIN_DRYRUN" = false ]; then
+        if [ -z "$PLUGIN_NAMESPACE" ]; then
+            kustomize build "${PLUGIN_FOLDERPATH}" | kubectl apply -f -
+        else
+            kustomize build "${PLUGIN_FOLDERPATH}" | kubectl apply -f - --namespace "$PLUGIN_NAMESPACE"
+        fi
+    fi
 fi
-
-if [ -n "$PLUGIN_KUBECONFIG" ];then
-    [ -d "$HOME"/.kube ] || mkdir "$HOME"/.kube  # uncomment post testing
-    echo "# Plugin PLUGIN_KUBECONFIG available" >&2
-    echo "$PLUGIN_KUBECONFIG" > "$HOME"/.kube/config # uncomment post testing
-    unset PLUGIN_KUBECONFIG
-fi
-
-[ -n "${PLUGIN_DEBUG:-false}" ] && kustomize build "${PLUGIN_FOLDERPATH}"
-
-if [ "$PLUGIN_DRYRUN" = false ]; then
-    kustomize build "${PLUGIN_FOLDERPATH}" | kubectl apply -f -
+if [[ "$PLUGIN_KUBECONFORM_BUILD" == "true" ]]; then
+    if [ -z "$PLUGIN_KUBECONFORM_PARAMETERS" ]; then
+        PLUGIN_KUBECONFORM_PARAMETERS="-skip Route"
+    fi
+    for DIR in $(echo "$PLUGIN_KUBECONFORM_PATHS" |tr , " "); do
+        echo "$DIR"
+        kustomize build "$DIR" | kubeconform -summary "$PLUGIN_KUBECONFORM_PARAMETERS"
+        echo "------------------"
+    done
 fi
